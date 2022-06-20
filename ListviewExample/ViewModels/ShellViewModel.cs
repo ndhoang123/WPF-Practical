b7016b31df6f88475b639cf3fbf4a2667685300d
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-06-20 22:55:56 +0700

    Custom the table

--- ListviewExample/ViewModels/ShellViewModel.cs
+++ ListviewExample/ViewModels/ShellViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ListviewExample.ViewModels
 {
@@ -13,6 +15,13 @@ namespace ListviewExample.ViewModels
 		private string _firstName = "Sergio";
 
 		private string _lastName;
+
+		public ShellViewModel()
+		{
+			People.Add(new PersonModel { FirstName = "Tim", LastName = "Corey", Age = 12 });
+			People.Add(new PersonModel { FirstName = "Bill", LastName = "Jonas", Age = 15 });
+			People.Add(new PersonModel { FirstName = "Sue", LastName = "Corey", Age = 21 });
+		}
 
 		public string FirstName
 		{
@@ -30,12 +39,12 @@ namespace ListviewExample.ViewModels
 
 		public string LastName
 		{
-			get 
-			{ 
-				return _lastName; 
+			get
+			{
+				return _lastName;
 			}
-			set 
-			{ 
+			set
+			{
 				_lastName = value;
 				NotifyOfPropertyChange(() => LastName);
 				NotifyOfPropertyChange(() => FullName);
@@ -55,5 +64,31 @@ namespace ListviewExample.ViewModels
 			set { _people = value; }
 		}
 
+	}
+	public class ScrollablePanel : StackPanel
+	{
+		protected override Size MeasureOverride(Size constraint)
+		{
+			Size tmpSize = base.MeasureOverride(constraint);
+			tmpSize.Height = (double)(this.Children[0] as UIElement).GetValue(MinHeightProperty);
+			return tmpSize;
+		}
+
+		protected override System.Windows.Size ArrangeOverride(System.Windows.Size finalSize)
+		{
+			Size tmpSize = new Size(0, 0);
+
+			//Width stays the same
+			tmpSize.Width = finalSize.Width;
+
+			//Height is changed
+			tmpSize.Height = finalSize.Height;
+
+			//This works only for one child!
+			this.Children[0].SetCurrentValue(HeightProperty, tmpSize.Height);
+			this.Children[0].Arrange(new Rect(new Point(0, 0), tmpSize));
+
+			return tmpSize;
+		}
 	}
 }
